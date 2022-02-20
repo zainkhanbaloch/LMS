e31f8b91baac495b4911e0b979fd7c59d1a188a6
MaazAhmad26 <100081006+MaazAhmad26@users.noreply.github.com>
2022-02-20 20:34:46 +0500

    sad

--- src/components/student-details-entry/index.js
+++ src/components/student-details-entry/index.js
@@ -4,7 +4,12 @@ import './style.css';
 const StudentDetailsEntry = () => {
     return <div className='col-12 d-flex flex-column align-items-center justify-content-center p-0 student-details'>
        <button className='button-submit'>
-Click here to start.
+Click here to start. <span className='fa fa-phone'></span>
+
+//Ask for student name
+//Ask for student age
+//Ask for student class
+//Button that says start entry
        </button>
     </div>
 };
